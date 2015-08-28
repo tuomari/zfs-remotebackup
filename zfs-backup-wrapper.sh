1f279b722d478af9e00374ae4f1ff654ab87b434
Tuomas Riihimäki <tuomari@iki.fi>
2015-08-28 18:42:55 +0300

    Add cleaup for backup-wrapper

--- zfs-backup-wrapper.sh
+++ zfs-backup-wrapper.sh
@@ -21,6 +21,42 @@ fi
 
 RCVCMD="$MBUFCMD | /usr/bin/sudo /sbin/zfs receive $BASEPATH/$ENDPATH"
 
+function cleanup {
+
+  echo "Running cleanup for $1 $2";
+  if [[ -z $1 || -z $2 ]] ; then
+     echo "TWO parameters required for cleanup: TARGET and SNAPTYPE"
+     return 1;
+  fi
+
+ TARGET=$1;
+ SNAPTYPE=$2
+ SAVECOUNT=$(/sbin/zfs get -H -o value "$SNAP_SAVECOUNT_PROPERTY$SNAPTYPE" "$TARGET");
+
+ # Default values for snapshots to save.
+ if [[ -z $SAVECOUNT || ! $SAVECOUNT =~ ^[0-9]+$ ]]; then
+   case "$SNAPTYPE" in
+     "frequent") SAVECOUNT=48 ;;
+     "daily") SAVECOUNT=31 ;;
+     "weekly") SAVECOUNT=8 ;;
+     "monthly") SAVECOUNT=12 ;;
+   esac
+ fi
+
+# We always want to save atleast one snapshot of a kind.
+#
+if [ $SAVECOUNT -le 0 ]; then
+ SAVECOUNT=1;
+fi
+
+ echo "Keeping latest $SAVECOUNT snapshots. destroying the rest from $TARGET with grep $SNAPNAME_PREFIX$SNAPTYPE$SNAPNAME_SUFFFIX";
+ for i in $(sudo /sbin/zfs list -H -o name -r -t snapshot "$TARGET" |grep "$SNAPNAME_PREFIX$SNAPTYPE$SNAPNAME_SUFFFIX" |head -n -$SAVECOUNT); do 
+   echo "Destroying $i";
+   sudo /sbin/zfs destroy "$i";
+ done
+
+}
+
 case "$COMMAND" in
 	list)
 		sudo zfs list -r -t all "$BASEPATH/$ENDPATH";
@@ -45,7 +81,13 @@ case "$COMMAND" in
 		eval "$RCVCMD";
 
 		;;
-        *)
+        cleanup)
+		SNAPNAME_PREFIX="$5";
+		SNAPNAME_SUFFFIX="$6";
+		SNAP_SAVECOUNT_PROPERTY="$7";
+		cleanup "$BASEPATH/$ENDPATH" "$3";
+ 		;;
+       *)
 		echo "Illegal command $COMMAND";
 		;;
 esac
